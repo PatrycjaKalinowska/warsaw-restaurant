289c3f5964f52a140459fcf4e27e3832a3d0fd9a
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-09-11 21:00:02 +0200

    added Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import './Navbar.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
 
     return <div className="navbar">
-        <header className="navbar__header">
-            <span className="navbar__logo">Warsaw Restaurant</span>
+            <div className="navbar__logo"><a className="navbar__logo-link" href="#">Warsaw Restaurant</a></div>
             <nav className="navbar__nav">
                 <ul className="navbar__menu-list">
-                    <li className="navbar__menu-item">About Us</li>
-                    <li className="navbar__menu-item">Menu</li>
-                    <li className="navbar__menu-item">Chef</li>
-                    <li className="navbar__menu-item">Gallery</li>
-                    <li className="navbar__menu-item">Find Us</li>
+                    <li className="navbar__menu-item"><a href="#">About Us</a></li>
+                    <li className="navbar__menu-item"><a href="#">Menu</a></li>
+                    <li className="navbar__menu-item"><a href="#">Chef</a></li>
+                    <li className="navbar__menu-item"><a href="#">Gallery</a></li>
+                    <li className="navbar__menu-item"><a href="#">Find Us</a></li>
                 </ul>
+                <div className="navbar__menu-hamburger">
+                    <GiHamburgerMenu color="#cfcdcd" fontSize={25} onClick={() => {setToggleMenu(true)}}/>
+
+                    {toggleMenu && (<div className="navbar__menu-hamburger-overlay">
+                    <FaTimes className="navbar__menu-hamburger-overlay-close" color="#cfcdcd" font-size={25} onClick={() => {setToggleMenu(false)}}/>
+                    <ul className="navbar__menu-hamburger-list">
+                        <li className="navbar__menu-hamburger-item"><a href="#">About Us</a></li>
+                        <li className="navbar__menu-hamburger-item"><a href="#">Menu</a></li>
+                        <li className="navbar__menu-hamburger-item"><a href="#">Chef</a></li>
+                        <li className="navbar__menu-hamburger-item"><a href="#">Gallery</a></li>
+                        <li className="navbar__menu-hamburger-item"><a href="#">Find Us</a></li>
+                    </ul>
+                </div>)}
+                     
+
+
+                </div>
             </nav>
-        </header>
     </div>
 
 }
